232f711ed1066e744ca2ad8a7a5256677cab5897
rahulrimlhash6 <rahulrimlhash6@gmaii.com>
2024-10-22 20:21:11 +0530

    model changes

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
+# Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.CharField(max_length=100)
